5e3e987ea5e79cef268c86d19987ed37419723dd
sebdsz <sebdesquirez@gmail.com>
2016-07-06 13:09:57 +0200

    visuel seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,7 +11,7 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 100)->create();
+        factory(App\Post::class, 30)->create();
 
         $dirUpload = public_path(env('UPLOAD_PICTURES', 'uploads'));
         $files = File::allFiles($dirUpload);
@@ -20,8 +20,9 @@ class PostsTableSeeder extends Seeder
         foreach (App\Post::all() as $post) {
             $uri = str_random(12) . '_370x235.jpg';
             $id = rand(1, 9);
+            rmdir($dirUpload . DIRECTORY_SEPARATOR . $post->id . DIRECTORY_SEPARATOR);
             mkdir($dirUpload . DIRECTORY_SEPARATOR . $post->id . DIRECTORY_SEPARATOR, 0700);
-            $fileName = file_get_contents('http://lorempicsum.com/nemo/370/235/' . $id);
+            $fileName = file_get_contents('http://lorempicsum.com/nemo/627/300/' . $id);
             File::put($dirUpload . DIRECTORY_SEPARATOR . $post->id . DIRECTORY_SEPARATOR . $uri, $fileName);
             $post->update([
                     'url_thumbnail' => $uri,

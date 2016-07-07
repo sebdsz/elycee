38fe0894c02768e731e851caeb3f4c3493ef51ed
sebdsz <sebdesquirez@gmail.com>
2016-07-07 16:55:14 +0200

    debug navbar

--- resources/views/partials/back/nav.blade.php
+++ resources/views/partials/back/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="sidebar-left">
     <ul>
-        <li class="{{ Request::is('admin')? 'active': '' }}"><a href="{{ action('BackController@index') }}">Dashboard</a></li>
-        <li class="{{ Request::is('admin/fiches*')? 'active': '' }}"><a href="{{ action('RecordController@index') }}">QCM</a></li>
-        <li class="{{ Request::is('admin/articles*')? 'active': '' }}"><a href="{{ action('PostController@index') }}">Articles</a></li>
-        <li class="{{ Request::is('admin/eleves*')? 'active': '' }}"><a href="{{ action('StudentController@index') }}">Élèves</a></li>
+        <li class="{{ Request::is('admin') ? 'active': '' }}"><a href="{{ action('BackController@index') }}">Dashboard</a></li>
+        <li class="{{ Request::is('admin/fiches*') || Request::is('admin/questions*') ? 'active': '' }}"><a href="{{ action('RecordController@index') }}">QCM</a></li>
+        <li class="{{ Request::is('admin/articles*') ? 'active': '' }}"><a href="{{ action('PostController@index') }}">Articles</a></li>
+        <li class="{{ Request::is('admin/eleves*') ? 'active': '' }}"><a href="{{ action('StudentController@index') }}">Élèves</a></li>
     </ul>
 </nav>

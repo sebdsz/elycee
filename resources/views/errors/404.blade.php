da592f1e51f30f1e4ea94578a8e90a3c405503d7
sebdsz <sebdesquirez@gmail.com>
2016-07-12 15:39:53 +0200

    page404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="page-404">
         <div>
-            <h2>Page 404</h2>
+            <h2>Page <span class="blue">4</span>0<span class="orange">4</span></h2>
             <h4>Cette page n'existe pas ou n'existe plus...</h4>
             <div class="row">
                 <div class="col-xs-12">

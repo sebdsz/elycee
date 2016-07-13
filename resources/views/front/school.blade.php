9d9f8ea161e475ac5977c30ec56df92b1fc45866
sebdsz <sebdesquirez@gmail.com>
2016-07-13 17:56:31 +0200

    css

--- resources/views/front/school.blade.php
+++ resources/views/front/school.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.front')
 
 @section('content')
-    <h2>Page du lycée (statique)</h2>
-
-    DEMMERDE TOI
+    <h2>Lycée</h2>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi consequuntur cupiditate, eum nobis
+        omnis pariatur placeat repellat sequi tempora. Aliquid animi architecto asperiores aspernatur dignissimos ea
+        earum id iste itaque iure laborum magnam molestiae odio, officiis provident, quae quod sint. Dolores ipsum
+        laboriosam natus omnis quibusdam similique sit sunt.
+    </div>
+    <div>Accusamus animi eaque, eveniet laborum magnam odio quam quia, quis, suscipit ullam vitae voluptas! Alias animi
+        commodi cum cumque delectus deleniti dolorum est eum id, in ipsa ipsam ipsum iure modi mollitia necessitatibus
+        non nostrum numquam optio perspiciatis porro quae quia ratione recusandae reprehenderit, sunt, suscipit
+        veritatis vitae voluptatem voluptatibus.
+    </div>
+    <div>Architecto asperiores atque commodi cumque debitis deleniti dicta distinctio dolores esse et excepturi, fuga
+        illum inventore iste iusto laboriosam magnam molestiae neque, non rem sequi, sit tenetur. Ab accusamus,
+        accusantium at atque dolorum et hic molestiae nam nihil perspiciatis recusandae reiciendis sunt totam unde vero!
+        Dignissimos dolorem eius quia rem!
+    </div>
 @endsection

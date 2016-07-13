3f3f6a059e1b9b4ac1d09c0f54a96a20706c2ff9
sebdsz <sebdesquirez@gmail.com>
2016-07-13 10:47:24 +0200

    css

--- resources/views/partials/back/nav-student.blade.php
+++ resources/views/partials/back/nav-student.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar-left">
     <ul>
-        <li class="{{ Request::is('etudiant') ? 'active': '' }}"><a href="{{ action('QCMController@dashboard') }}">Dashboard</a></li>
-        <li class="{{ Request::is('etudiant/liste') || Request::is('etudiant/fiche*') ? 'active': '' }}"><a href="{{ action('QCMController@index') }}">Questions</a></li>
+        <li class="{{ Request::is('etudiant') ? 'active': '' }}"><a href="{{ action('QCMController@dashboard') }}"><span class="glyphicon glyphicon-stats"></span> Dashboard</a></li>
+        <li class="{{ Request::is('etudiant/liste') || Request::is('etudiant/fiche*') ? 'active': '' }}"><a href="{{ action('QCMController@index') }}"><span class="glyphicon glyphicon-education"></span> Questions</a></li>
     </ul>
 </nav>
--- resources/views/partials/back/nav.blade.php
+++ resources/views/partials/back/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="sidebar-left">
     <ul>
-        <li class="{{ Request::is('admin') ? 'active': '' }}"><a href="{{ action('BackController@index') }}">Dashboard</a></li>
-        <li class="{{ Request::is('admin/fiches*') || Request::is('admin/questions*') ? 'active': '' }}"><a href="{{ action('RecordController@index') }}">Questions</a></li>
-        <li class="{{ Request::is('admin/articles*') ? 'active': '' }}"><a href="{{ action('PostController@index') }}">Articles</a></li>
-        <li class="{{ Request::is('admin/eleves*') ? 'active': '' }}"><a href="{{ action('StudentController@index') }}">Élèves</a></li>
+        <li class="{{ Request::is('admin') ? 'active': '' }}"><a href="{{ action('BackController@index') }}"><span class="glyphicon glyphicon-stats"></span> Dashboard</a></li>
+        <li class="{{ Request::is('admin/fiches*') || Request::is('admin/questions*') ? 'active': '' }}"><a href="{{ action('RecordController@index') }}"><span class="glyphicon glyphicon-education"></span> Questions</a></li>
+        <li class="{{ Request::is('admin/articles*') ? 'active': '' }}"><a href="{{ action('PostController@index') }}"><span class="glyphicon glyphicon-file"></span> Articles</a></li>
+        <li class="{{ Request::is('admin/eleves*') ? 'active': '' }}"><a href="{{ action('StudentController@index') }}"><span class="glyphicon glyphicon-user"></span> Élèves</a></li>
     </ul>
 </nav>
